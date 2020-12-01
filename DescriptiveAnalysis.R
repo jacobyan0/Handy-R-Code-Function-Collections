ba345d990bd0060b7b1406a6769d62a406edfbfe
jacobyan0 <41859032@qq.com>
2020-12-01 12:16:49 -0500

    Create DescriptiveAnalysis.R

---
+++ DescriptiveAnalysis.R
@@ -0,0 +1,20 @@
+## Delete NA
+data<-data[complete.cases(data$attribute),]
+
+### Correlation matrix
+cor(data,use="complete.obs")
+
+### Histograms for all variables
+library(purrr)
+library(tidyr)
+library(ggplot2)
+
+WA_HousingVar %>%
+  keep(is.numeric) %>% 
+  gather() %>% 
+  ggplot(aes(value)) +
+  facet_wrap(~ key, scales = "free") +
+  geom_histogram()
+
+### scattorplots for all pairs
+pairs(WA_HousingVar[,c("x1","x2")],pch=19,lower.panel = NULL)
